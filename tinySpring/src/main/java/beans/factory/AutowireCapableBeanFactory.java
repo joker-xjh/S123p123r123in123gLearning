d22185496a6dfad5e3c3aaf1da8ec2d3ee844f99
joker_xjh <1781934063@qq.com>
2017-09-25 17:48:06 +0800

    to be continued

--- tinySpring/src/main/java/beans/factory/AutowireCapableBeanFactory.java
+++ tinySpring/src/main/java/beans/factory/AutowireCapableBeanFactory.java
@@ -16,17 +16,18 @@ public class AutowireCapableBeanFactory extends AbstractBeanFactory{
 		
 		for(PropertyValue propertyValue : beanDefinition.getPropertyValues().getPropertyValues()) {
 			Object value = propertyValue.getValue();
-			
+			Class<?> type = bean.getClass().getDeclaredField(propertyValue.getName()).getType();
 			if(value instanceof BeanReference) {
 				BeanReference beanReference = (BeanReference)value;
 				value = getBean(beanReference.getName());
 			}
-			
+			else {
+				value = parseType(value, type);
+			}
 			//调用setXXX方法设置属性
-			
 			Method method = bean.getClass().getDeclaredMethod(
 					"set" + propertyValue.getName().substring(0, 1).toUpperCase() + propertyValue.getName().substring(1)
-					, value.getClass());
+					, type);
 			boolean b = method.isAccessible();
 			method.setAccessible(true);
 			method.invoke(bean, value);
@@ -34,5 +35,31 @@ public class AutowireCapableBeanFactory extends AbstractBeanFactory{
 		}
 		
 	}
+	
+	protected Object parseType(Object value, Class<?> type) throws Exception{
+		if(type.equals(int.class) || type.equals(Integer.class)) {
+			return Integer.parseInt(value.toString());
+		}
+		else if(type.equals(Long.class) || type.equals(long.class)) {
+			return Long.parseLong(value.toString());
+		}
+		else if(type.equals(double.class) || type.equals(Double.class)) {
+			return Double.parseDouble(value.toString());
+		}
+		else if(type.equals(Boolean.class) || type.equals(boolean.class)) {
+			return Boolean.parseBoolean(value.toString());
+		}
+		else if(type.equals(char.class) || type.equals(Character.class)) {
+			return value.toString().charAt(0);
+		}
+		else if(type.equals(String.class)) {
+			return value.toString();
+		}
+		return value;
+	}
+	
+	
+	
+	
 
 }
